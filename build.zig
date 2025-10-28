b3bc3c76de8afcfedd2cad700f833df08a817c51
Kbz-8 <kbz_8.dev@akel-engine.com>
2025-10-28 09:06:21 +0100

    Initial commit

---
+++ build.zig
@@ -0,0 +1,29 @@
+const std = @import("std");
+
+pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const lib_mod = b.createModule(.{
+        .root_source_file = b.path("src/lib.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+
+    const lib = b.addLibrary(.{
+        .name = "vulkan",
+        .root_module = lib_mod,
+        .linkage = .dynamic,
+    });
+
+    b.installArtifact(lib);
+
+    const lib_tests = b.addTest(.{
+        .root_module = lib_mod,
+    });
+
+    const run_lib_tests = b.addRunArtifact(lib_tests);
+
+    const test_step = b.step("test", "Run tests");
+    test_step.dependOn(&run_lib_tests.step);
+}
